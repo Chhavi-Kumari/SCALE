ff57453e1db75818ec7667d01f7434551edfb2bb
Chhavi-Kumari <214402125+Chhavi-Kumari@users.noreply.github.com>
2026-07-09 15:53:45 -0700

    Fix formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,9 @@ const nextConfig: NextConfig = {
   },
   env: {
     AUTH_APPS_SCRIPT_URL: process.env.AUTH_APPS_SCRIPT_URL,
-    SESSION_SECRET: process.env.SESSION_SECRET
+    SESSION_SECRET: process.env.SESSION_SECRET,
     ARTICLES_CSV_URL: process.env.ARTICLES_CSV_URL
 
   }
 };
-
 export default nextConfig;
